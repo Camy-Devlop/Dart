2ad1a63901176381593e2c384c6005b21e992191
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2023-03-07 21:10:01 +0100

    Update Etudiant.dart
    ajout paramètre dans le constructor

--- Etudiant.dart
+++ Etudiant.dart
@@ -1,42 +1,43 @@
-
-
-class Etudiant
-{
+class Etudiant {
   String? nom;
   String? prenom;
+  final DateTime? dateNaissance;
+  String? adresse;
+  String? email;
   List<double>? points;
-  
-  Etudiant(this.nom,this.prenom);
-  
-  void add_point(double a)
-  { 
-    if(this.points!=null)
-    {  
+
+  Etudiant(this.nom, this.prenom,
+      {this.points, this.dateNaissance, this.adresse, this.email});
+
+  void add_point(double a) {
+    if (this.points != null) {
       this.points!.add(a);
-    }else{
-      this.points=[a];
+    } else {
+      this.points = [a];
     }
   }
-  
-  void presentation()
-  {
+
+  void presentation() {
     print("Je m'appelle ${this.nom} ${this.prenom}");
   }
-  
-  void total_point_100()
-  {
-    int nombre;
-    double notes=0;
-    
-    if(this.points!=null)
-    {
-      this.points!.forEach((element){
-        notes+=element;
+
+  String nomPrenom() {
+    return this.nom! + " " + this.prenom!;
+  }
+
+  double total_point_100() {
+    double notes = 0;
+
+    if (this.points != null) {
+      this.points!.forEach((element) {
+        notes += element;
       });
     }
-    
-    print(this.points!=null?notes:"il y pas de points!!!");
-    
+    return this.points!.length > 0 ? notes / this.points!.length : 0;
+    //print(this.points != null ? notes : "il y pas de points!!!");
+  }
+
+  String toString() {
+    return (this.nom == null || this.prenom == null) ? "" : nomPrenom();
   }
 }
-
